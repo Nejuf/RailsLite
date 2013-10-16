7137bb908be2938fbb563bb54ca1c75373b0ea24
Nejuf <matthew.b.blankenship@gmail.com>
2013-10-16 11:23:17 -0700

    Adds rendering to ControllerBase

--- lib/rails_lite/controller_base.rb
+++ lib/rails_lite/controller_base.rb
@@ -1,4 +1,5 @@
 require 'erb'
+require 'active_support/core_ext'
 require_relative 'params'
 require_relative 'session'
 
@@ -21,19 +22,35 @@ public
   end
 
   def redirect_to(url)
-    #set status to 302/redirect
-    #set location header to new url
+    #sets status to 302/redirect Found 
+    #not 301(Moved Permanently) because some browsers will cache the redirect
+    #sets location header to new url
+    @already_built_response = true
+    #redirect_status = WEBrick::HTTPStatus::TemporaryRedirect
+    #@res.set_redirect(redirect_status, url)
+    @res.header['location'] = url
+    @res.status = 302
   end
 
   def render_content(content, type)
+    @already_built_response = true
     @res.content_type = type
     @res.body = content
   end
 
   def render(template_name)
     #fill in response body
+    controller_name = get_controller_name
+    template_contents = File.read("view/#{controller_name}/#{template_name}.html.erb")
+    
+    erb = ERB.new(template_contents).result(binding)
+    render_content(erb, "text/html")
   end
 
   def invoke_action(name)
+  end
+
+  def get_controller_name
+    self.class.to_s.reverse.gsub("Controller".reverse, "").reverse.underscore.downcase
   end
 end
--- test/00_server.rb
+++ test/00_server.rb
@@ -6,15 +6,17 @@ require_relative '../lib/rails_lite'
 # http://www.ruby-doc.org/stdlib-2.0/libdoc/webrick/rdoc/WEBrick/HTTPRequest.html
 # http://www.ruby-doc.org/stdlib-2.0/libdoc/webrick/rdoc/WEBrick/HTTPResponse.html
 # http://www.ruby-doc.org/stdlib-2.0/libdoc/webrick/rdoc/WEBrick/Cookie.html
-server = WEBrick::HTTPServer.new :Port => 8080
+root = File.expand_path '~/public_html'
+server = WEBrick::HTTPServer.new(:Port => 8080, :DocumentRoot => root)
 trap('INT') { server.shutdown }
 
 class MyController < ControllerBase
   def go
-    render_content("hello world!", "text/html")
-
+    #render_content("hello world!", "text/html")
+		#render_content(req.path, "text/html")
+		#redirect_to("http://www.google.com")
     # after you have template rendering, uncomment:
-#    render :show
+    render :show
 
     # after you have sessions going, uncomment:
 #    session["count"] ||= 0
