d0fbba45c509a71a272d3b6a53a687ad28d349e2
Nejuf <matthew.b.blankenship@gmail.com>
2013-10-16 11:46:29 -0700

    Adds sessions cookie

--- lib/rails_lite/controller_base.rb
+++ lib/rails_lite/controller_base.rb
@@ -16,6 +16,7 @@ public
   end
 
   def session
+    @session ||= Session.new(req)
   end
 
   def already_rendered?
@@ -30,18 +31,20 @@ public
     #@res.set_redirect(redirect_status, url)
     @res.header['location'] = url
     @res.status = 302
+    session.store_session(res)
   end
 
   def render_content(content, type)
     @already_built_response = true
     @res.content_type = type
     @res.body = content
+    session.store_session(res)
   end
 
   def render(template_name)
     #fill in response body
     controller_name = get_controller_name
-    template_contents = File.read("view/#{controller_name}/#{template_name}.html.erb")
+    template_contents = File.read("views/#{controller_name}/#{template_name}.html.erb")
     
     erb = ERB.new(template_contents).result(binding)
     render_content(erb, "text/html")
--- lib/rails_lite/session.rb
+++ lib/rails_lite/session.rb
@@ -2,15 +2,28 @@ require 'json'
 require 'webrick'
 
 class Session
+	COOKIE_NAME = "_rails_lite_app"
   def initialize(req)
+  	cookie = req.cookies.select{ |cookie| cookie.name == COOKIE_NAME }.first
+		if cookie.nil?
+			@data = {}
+		else
+			@data = JSON.parse(cookie.value)
+		end
   end
 
+  #e.g. session[:session_token]
   def [](key)
+  	@data[key]
   end
 
+  #e.g. session[:session_token] = nil
   def []=(key, val)
+  	@data[key] = val
   end
 
   def store_session(res)
+  	cookie = WEBrick::Cookie.new(COOKIE_NAME, @data.to_json)
+  	res.cookies << cookie
   end
 end
